d7640d4713b65bf5a16472ee9a9c2dae15da94af
Simon-Olivier Morneau <simonolivier.morneau@ulb.be>
2023-03-16 16:54:42 +0100

    Implement test client-server connection

--- t/relay_test_client.c
+++ t/relay_test_client.c
@@ -1,4 +1,5 @@
 #include "rapido.h"
+#include "rapido_internals.h"
 #include "picotls.h"
 #include "picotls/openssl.h"
 #include "util.h"
@@ -6,27 +7,49 @@
 #include <stdio.h>
 #include <sys/socket.h>
 
+#define RUN_NETWORK_TIMEOUT 100
+
 void ctx_load_cert(ptls_context_t *ctx, const char* cert_file);
 void ctx_add_sign_cert(ptls_context_t *ctx, const char* pk_file);
 
 int main(int argc, char *argv[]) {
     ptls_context_t ctx;
-    struct sockaddr_storage sa;
-    socklen_t salen;
-    const char *host = "127.0.0.1", *port = "8443";
+    struct sockaddr_storage sa, la;
+    socklen_t salen, lalen;
+    const char *host = "localhost", *port = "8443";
 
     memset(&ctx, 0, sizeof(ctx));
     ctx.random_bytes = ptls_openssl_random_bytes;
     ctx.key_exchanges = ptls_openssl_key_exchanges;
     ctx.cipher_suites = ptls_openssl_cipher_suites;
+    ctx.get_time = &ptls_get_time;
 
-    if (resolve_address((struct sockaddr *)&sa, &salen, host, port, AF_INET, SOCK_STREAM, IPPROTO_TCP) != 0) {
+    if (resolve_address((struct sockaddr *)&sa, &salen, host, port, AF_INET6, SOCK_STREAM, IPPROTO_TCP) != 0) {
         exit(1);
     }
 
     rapido_session_t *session = rapido_new_session(&ctx, false, host, stderr);
     rapido_address_id_t remote_addr = rapido_add_remote_address(session, (struct sockaddr *)&sa, salen);
-    rapido_create_connection(session, 0, remote_addr);
+    // rapido_address_id_t local_addr = rapido_add_address(session, (struct sockaddr *)&la, salen);
+    rapido_connection_id_t conn = rapido_create_connection(session, 0, remote_addr);
 
-    
+    rapido_application_notification_t* notification;
+    char *data;
+    size_t readlen = 64;
+
+    while (!session->is_closed) {
+        rapido_run_network(session, RUN_NETWORK_TIMEOUT);
+        while (session->pending_notifications.size > 0) {
+            notification = rapido_queue_pop(&session->pending_notifications);
+            if (notification->notification_type == rapido_new_stream) {
+                fprintf(stdout, "Received a new stream with ID %x\n", notification->stream_id);
+            } else if (notification->notification_type == rapido_stream_has_data) {
+                fprintf(stdout, "Stream %x has buffered data\n", notification->stream_id);
+                data = rapido_read_stream(session, notification->stream_id, &readlen);
+                fprintf(stdout, "Received data: %s\n", data);
+            } else if (notification->notification_type == rapido_stream_closed) {
+                fprintf(stdout, "No more data available in stream %x, now closed.", notification->stream_id);
+            }
+        }
+    }
 }
--- t/relay_test_server.c
+++ t/relay_test_server.c
@@ -1,4 +1,5 @@
 #include "rapido.h"
+#include "rapido_internals.h"
 #include "picotls.h"
 #include "picotls/openssl.h"
 #include "util.h"
@@ -7,7 +8,6 @@
 #include <sys/socket.h>
 
 #define RUN_NETWORK_TIMEOUT 100
-#define SESSION(server, index) ((rapido_session_t *)rapido_array_get(&(server)->sessions, (index)))
 
 void ctx_load_cert(ptls_context_t *ctx, const char* cert_file);
 void ctx_add_sign_cert(ptls_context_t *ctx, const char* pk_file);
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
     ptls_context_t ctx;
     struct sockaddr_in sa;
     socklen_t salen;
-    const char *host = "127.0.0.1", *port = "8443";
+    const char *host = "localhost", *port = "8443";
 
     memset(&ctx, 0, sizeof(ctx));
     ctx.random_bytes = ptls_openssl_random_bytes;
@@ -28,31 +28,41 @@ int main(int argc, char *argv[]) {
     ctx_load_cert(&ctx, "dev_certs/cert.pem");
     ctx_add_sign_cert(&ctx, "dev_certs/key.pem");
 
-    if (resolve_address((struct sockaddr *)&sa, &salen, host, port, AF_INET, SOCK_STREAM, IPPROTO_TCP) != 0) {
+    if (resolve_address((struct sockaddr *)&sa, &salen, host, port, AF_INET6, SOCK_STREAM, IPPROTO_TCP) != 0) {
         exit(1);
     }
 
-    rapido_server_t* server = rapido_new_server(&ctx, "127.0.0.1", stderr);
+    rapido_server_t* server = rapido_new_server(&ctx, "localhost", stderr);
     rapido_address_id_t server_addr = rapido_add_server_address(server, (struct sockaddr *)&sa, salen, true);
 
     size_t server_session_index;
-
     rapido_application_notification_t *notification = NULL;
+    rapido_stream_id_t active_server_stream_id;
+    rapido_session_t* server_session;
+
+    const char *test_payload = "Hello World!";
     
     fprintf(stderr, "Waiting for connections...");
 
-    while (!notification) {
-    rapido_run_server_network(server, RUN_NETWORK_TIMEOUT);
+    while (1) {
+        rapido_run_server_network(server, RUN_NETWORK_TIMEOUT);
         notification = rapido_next_server_notification(server, &server_session_index);
-        sleep(1);
+        if (notification && notification->notification_type == rapido_new_connection) {
+            fprintf(stdout, "Accepting a connection\n");
+            fprintf(stdout, "SessionID = %zd\n", server_session_index);
+            server_session = ((rapido_session_t *) rapido_array_get(&(server->sessions), server_session_index));
+            fprintf(stdout, "Session is_closed = %d\n", server_session->is_closed);
+            active_server_stream_id = rapido_open_stream(server_session);
+            rapido_add_to_stream(server_session, active_server_stream_id, test_payload, strlen(test_payload));
+            rapido_close_stream(server_session, active_server_stream_id);
+            rapido_attach_stream(server_session, active_server_stream_id, notification->connection_id);
+            rapido_run_network(server_session, RUN_NETWORK_TIMEOUT);
+            rapido_close_session(server_session, notification->connection_id);
+        } else {
+            fprintf(stdout, "Nope, still waiting...\n");
+        }
+        sleep(2);
     }
-
-    rapido_session_t *server_session = SESSION(server, server_session_index);
-    rapido_stream_id_t server_stream_id = rapido_open_stream(server_session);
-
-    rapido_add_to_stream(server_session, server_stream_id, "Hello", 6);
-    rapido_close_stream(server_session, server_stream_id);
-    rapido_attach_stream(server_session, server_stream_id, notification->connection_id);
 
     rapido_server_free(server);
     free(server);
